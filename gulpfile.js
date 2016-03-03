9dab49933ea05732695da4452ce2a4a33632f435
Paul Breslin <paul.breslin@outlook.com>
2016-03-03 23:02:35 +0000

    improve gulp tools

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,14 @@ var gulp = require('gulp'),
     watch = require('gulp-watch'),
     sass = require('gulp-sass'),
     livereload = require('gulp-livereload'),
-    webserver = require('gulp-webserver');
+    webserver = require('gulp-webserver'),
+    sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function() {
     gulp.src('./sass/**/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('./css'))
         .pipe(livereload());
 });
@@ -20,7 +23,8 @@ gulp.task('serve', function() {
     gulp.src('.')
         .pipe(webserver({
             livereload: true,
+            // host: '0.0.0.0',
         }));
 });
 
-gulp.task('default', ['serve', 'sass:watch']);
+gulp.task('default', ['sass', 'sass:watch', 'serve']);
